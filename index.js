c8e393c5c334cc716dabb457ec8fff788fee3256
Kristina Diaz <kristina.diaz84@gmail.com>
2023-05-03 11:56:09 -0600

    finished part 1 of rpg

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
-document.getElementById('hero').innerHTML = `
-<div class="character-card">
-    <h4 class="name"> Wizard </h4>
-        <img class="avatar" src="images/wizard.png"/>
-        <p class="health">health: <b> 60 </b></p>
-    <div class="dice-container"><div class="dice"> 6 </div></div>
-</div>
-`
-document.getElementById('monster').innerHTML = `
-<div class="character-card">
-    <h4 class="name"> Orc </h4>
-    <img class="avatar" src="images/orc.png"/>
-    <p class="health">health: <b> 10 </b></p>
-    <div class="dice-container"><div class="dice"> 4 </div></div>
-</div>
-`
+function getDiceRollArray(diceCount) {
+    return new Array(diceCount).fill(0).map(function(){
+        return Math.floor(Math.random() * 6) + 1
+    })    
+}
+
+function getDiceHtml(diceCount) {
+    return getDiceRollArray(diceCount).map(function(num){ 
+        return  `<div class="dice">${num}</div>`
+    }).join('')
+}
+
+const hero = {
+    elementId: "hero",
+    name: "Wizard",
+    avatar: "images/wizard.png",
+    health: 60,
+    diceCount: 3
+}
+
+const monster = {
+    elementId: "monster",
+    name: "Orc",
+    avatar: "images/orc.png",
+    health: 10,
+    diceCount: 1
+}
+
+function renderCharacter(data) {
+    const { elementId, name, avatar, health, diceCount } = data;
+    const diceHtml = getDiceHtml(diceCount)
+
+    document.getElementById(elementId).innerHTML =
+        `<div class="character-card">
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}" />
+            <div class="health">health: <b> ${health} </b></div>
+            <div class="dice-container">    
+                ${diceHtml}
+            </div>
+        </div>`;
+}
+
+renderCharacter(hero);
+renderCharacter(monster);
